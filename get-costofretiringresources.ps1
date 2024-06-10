7dacba386baa7a39993184365d95d2192aa1e848
Marcello Formica <mformica@microsoft.com>
2024-06-10 09:23:57 +0200

    v1.1.2 (#4)
    * fix endDate handling; moved API invocation to a separate internal function
    
    * code improvements
    
    * more accurate handling of 429 (throttling)
    
    * minor formatting changes for delay
    
    * Improved API request handling for 429 errors

--- get-costofretiringresources.ps1
+++ get-costofretiringresources.ps1
@@ -1,4 +1,4 @@
-# v1.1.1
+# v1.1.2
 
 [CmdletBinding()]
 param(
@@ -7,17 +7,116 @@ param(
     [Parameter(Mandatory = $true, HelpMessage = "Billing period, example: 202404")]
     [string]$billingPeriod = $null,
     [Parameter(Mandatory = $false, HelpMessage = "End date with format: YYYY-MM-DD")]
-    [datetime]$endDate = $null
+    [datetime]$endDate = [datetime]::MaxValue
 )
 
 #requires -version 7
 Set-StrictMode -Version Latest
 $ErrorActionPreference = 'Stop'
 
-$totalCost = 0
+<#
+.SYNOPSIS
+Get the cost of retiring resources in a billing period
+
+.PARAMETER resourceId
+The resource ID of the resource to get the cost for
+
+.PARAMETER billingPeriodStart
+The start of the billing period in UTC format
+
+.PARAMETER billingPeriodEnd
+The end of the billing period in UTC format
+
+.PARAMETER token
+The token to use for authentication
+#>
+function Get-ResourceCost($resourceId, $billingPeriodStart, $billingPeriodEnd, $token) {
+
+    $subscriptionId = $resourceId.Split("/")[2]
+    $resourceGroup = $resourceId.Split("/")[4]
+
+    $uri = "https://management.azure.com/subscriptions/${subscriptionId}/resourceGroups/${resourceGroup}/providers/Microsoft.CostManagement/query?api-version=2023-11-01"
+
+    $jsonPayload = @{
+        type       = "ActualCost"
+        dataSet    = @{
+            granularity = "Monthly"
+            aggregation = @{
+                totalCost = @{
+                    name     = "Cost"
+                    function = "Sum"
+                }
+            }
+            sorting     = @(@{
+                    direction = "ascending"
+                    name      = "UsageDate"
+                })
+            filter      = @{
+                Dimensions = @{
+                    Name     = "ResourceId"
+                    Operator = "In"
+                    Values   = @("${resourceId}")
+                }
+            }
+        }
+        timeframe  = "Custom"
+        timePeriod = @{
+            from = "${billingPeriodStart}"
+            to   = "${billingPeriodEnd}"
+        }
+    }
+
+    $jsonPayload = $jsonPayload | ConvertTo-Json -Depth 10
+
+    $cost = 0
+
+    # send the request. Handle errors 429 (too many requests)
+    do {
+        $requestCompleted = $false
+        $statusCode = 0
+        $responseHeaders = $null
+        $response = Invoke-RestMethod -Uri $uri -Method Post -SkipHttpErrorCheck -StatusCodeVariable "statusCode" -ResponseHeadersVariable "responseHeaders" -Body $jsonPayload -Headers @{
+            'Authorization' = "Bearer $token"
+            'Content-Type'  = 'application/json'
+            "Client-Type"   = "GetCostOfRetiringResources"
+        }
+
+        if ($statusCode -eq 429) {
+            $qpuRetryAfter = [double]::Parse($responseHeaders['x-ms-ratelimit-microsoft.costmanagement-client-qpu-retry-after'] ?? 0)
+            $clientRetryAfter = [double]::Parse($responseHeaders['x-ms-ratelimit-microsoft.costmanagement-client-retry-after'] ?? 0)
+            $tenantRetryAfter = [double]::Parse($responseHeaders['x-ms-ratelimit-microsoft.costmanagement-tenant-retry-after'] ?? 0)
+            $entityRetryAfter = [double]::Parse($responseHeaders['x-ms-ratelimit-microsoft.costmanagement-entity-retry-after'] ?? 0)            
+            $retryAfterSet = @($qpuRetryAfter, $clientRetryAfter, $tenantRetryAfter, $entityRetryAfter)
+            $retryAfter = $retryAfterSet | Sort-Object -Descending | Select-Object -First 1 # get the maximum value of the retry-after values
+            if ($retryAfter -eq 0) {
+                $retryAfter = 30    # if none of the above is set, assume a delay of 30 seconds
+            }
+
+            write-host -ForegroundColor DarkYellow "wait ${retryAfter}s... " -NoNewline
+            Start-Sleep -Seconds $retryAfter
+        }
+        elseif ($statusCode -ne 200) {
+            Write-host -ForegroundColor Red "Error: $statusCode"
+            $requestCompleted = $true
+        }
+        else {
+            if (($null -ne $response.properties.rows) -and ($response.properties.rows.Count -gt 0)) {
+                $cost = $response.properties.rows[0][0]
+                write-host -ForegroundColor Yellow ("{0:N5}" -f $cost)
+            }
+            else {
+                write-host -ForegroundColor Green "no data"
+            }
+            $requestCompleted = $true
+        }
+    } while (-not $requestCompleted)
+
+    return $cost
+}
+
+### main script ###
+
 $currentDate = Get-Date
-# prepare a hashtable to store cumulative costs for each resource type
-$totalCostByResourceType = @{}
 
 # Validate billing period
 if ($billingPeriod -notmatch '^\d{6}$') {
@@ -44,23 +143,20 @@ if ($null -eq $resourceIds) {
 # convert the retirement date to a datetime object
 $resourceIds = $resourceIds | % { $_.'Retirement Date' = ( $_.'Retirement Date' -as [datetime] ) ; $_ }
 
-# sort by retirement date (soonest first) then by retiring feature
-$resourceIds = $resourceIds | Sort-Object -Property @{Expression = "Retirement Date"; Ascending = $true }, @{Expression = "Retiring Feature"; Ascending = $true }
-
-# if an end date is specified, filter out resources with a retirement date after the end date
-if (-not [string]::IsNullOrEmpty($endDate)) {
-    $resourceIds = $resourceIds | Where-Object { $_.'Retirement Date' -le $endDate }
-}
+# filter out resource whose retirement date is before today or after the end date (if specified, otherwise assume [datetime]::MaxValue)
+$resourceIds = $resourceIds | Where-Object { $_.'Retirement Date' -ge $currentDate -and $_.'Retirement Date' -le $endDate }
 
 if ($null -eq $resourceIds -or $resourceIds.Count -eq 0) {
-    Write-Error "No resources found in file with future retirement date on or before ${endDate}"
+    if ([datetime]::MaxValue -ne $endDate) {
+        Write-Error "No resources found in file with future retirement date on or before ${endDate}."
+    }
+    else {
+        Write-Error "No resources found in file with future retirement date."
+    }
     return
 }
 
-# filter out resources with a retirement date in the past
-$resourceIds = $resourceIds | Where-Object { $_.'Retirement Date' -ge ($currentDate.ToString("yyyy-MM-dd")) }
-
-Write-Host -NoNewline ([string]::IsNullOrEmpty($endDate) ? "Resources in file with future retirement date: " : "Resources in file with future retirement date on or before ${endDate}: ")
+Write-Host -NoNewline ([datetime]::MaxValue -eq $endDate ? "Resources in file with future retirement date: " : "Resources in file with future retirement date on or before ${endDate}: ")
 Write-Host -ForegroundColor Yellow $resourceIds.Count
 
 # if we have ASE in the list, we also need to get the list of all app plans in the ASE
@@ -81,7 +177,6 @@ if ($null -ne $aseResources) {
         | where ASEid <> ''
 "@
 
-    #$appPlans = Get-AzAppServicePlan -ProgressAction Ignore | Where-Object HostingEnvironmentProfile -ne $null  # NOT USED, it gets app plans only from the current subscription
     $impactedAppPlans = $appPlans | Where-Object { $aseResourcesIds -contains $_.ASEid }
 
     if ($null -eq $impactedAppPlans) {
@@ -112,93 +207,36 @@ if ($null -ne $aseResources) {
 # get a token
 $token = (Get-AzAccessToken -ResourceUrl "https://management.azure.com/").Token
 
-# iterate over the resources in the list
-$line = 0
-Write-Host
+# sort by retirement date (soonest first) then by retiring feature
+$resourceIds = $resourceIds | Sort-Object -Property @{Expression = "Retirement Date"; Ascending = $true }, @{Expression = "Retiring Feature"; Ascending = $true }
 
+$resourceCount = 0
+$totalCost = 0
+# prepare a hashtable to store cumulative costs for each resource type
+$totalCostByResourceType = @{}
+
+# iterate over the resources in the list
+Write-Host
 foreach ($resourceLine in $resourceIds) {
 
-    $line++
+    $resourceCount++
 
     $resourceId = $resourceLine.'Resource Name'
-    $subscriptionId = $resourceId.Split("/")[2]
     $resourceType = $resourceLine.'Type'
     # extract the resource name from the resource ID - it's the last part of the resource ID from item #7 onwards. It can have one or more slashes in it.
     $resourceName = $resourceid.Split("/")[8..($resourceid.Split("/").Count - 1)] -join "/"
-    $resourceGroup = $resourceid.Split("/")[4]
     $retirementDate = $resourceLine.'Retirement Date'
     $retiringFeature = $resourceLine.'Retiring Feature'
 
-    # get the cost for the resource in the billing period
-    write-host -NoNewline "[${line}] "
+    write-host -NoNewline "[${resourceCount}] "
     write-host -NoNewline -ForegroundColor Cyan "${retirementDate} "
     write-host -NoNewline "${resourceType}, "
     write-host -NoNewline -ForegroundColor Yellow "${retiringFeature}: "
     write-host -NoNewLine "${resourceName}: "
 
-    $uri = "https://management.azure.com/subscriptions/${subscriptionId}/resourceGroups/${resourceGroup}/providers/Microsoft.CostManagement/query?api-version=2023-11-01"
-    
-    $jsonPayload = @{
-        type       = "ActualCost"
-        dataSet    = @{
-            granularity = "Monthly"
-            aggregation = @{
-                totalCost = @{
-                    name     = "Cost"
-                    function = "Sum"
-                }
-            }
-            sorting     = @(@{
-                    direction = "ascending"
-                    name      = "UsageDate"
-                })
-            filter      = @{
-                Dimensions = @{
-                    Name     = "ResourceId"
-                    Operator = "In"
-                    Values   = @("${resourceId}")
-                }
-            }
-        }
-        timeframe  = "Custom"
-        timePeriod = @{
-            from = "${billingPeriodStart}"
-            to   = "${billingPeriodEnd}"
-        }
-    }
-    
-    $jsonPayload = $jsonPayload | ConvertTo-Json -Depth 10
-    
-    $cost = 0
+    # get the cost for the resource in the billing period
+    $cost = Get-ResourceCost -resourceId $resourceId -billingPeriodStart $billingPeriodStart -billingPeriodEnd $billingPeriodEnd -token $token
 
-    # send the request. Handle errors 429 (too many requests) by waiting 30 seconds and retrying
-    do {
-        $done = $false
-        $statusCode = 0
-        $response = Invoke-RestMethod -Uri $uri -Method Post -SkipHttpErrorCheck -StatusCodeVariable "statusCode" -Body $jsonPayload -Headers @{
-            'Authorization' = "Bearer $token"
-            'Content-Type'  = 'application/json'
-        }
-        if ($statusCode -eq 429) {
-            write-host -ForegroundColor DarkYellow "wait... " -NoNewline
-            Start-Sleep -Seconds 30
-        }
-        elseif ($statusCode -ne 200) {
-            Write-host -ForegroundColor Red "Error: $statusCode"
-            $done = $true
-        }
-        else {
-            if (($null -ne $response.properties.rows) -and ($response.properties.rows.Count -gt 0)) {
-                $cost = $response.properties.rows[0][0]
-                write-host -ForegroundColor Yellow ("{0:N5}" -f $cost)            
-            }
-            else {
-                write-host -ForegroundColor Green "no data"
-            }
-            $done = $true
-        }
-    } while (-not $done)
-    
     $totalCost += $cost
     $totalCostByResourceType["${resourceType}, ${retiringFeature}"] += $cost
 }
